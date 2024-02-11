1875867496e9cf83b41e7a900d4cb19a09b9d224
4eck-qed <ssindustries@web.de>
2024-02-11 02:52:31 +0100

    fix highlights and devicons lazy load issues

--- lua/baron/plugins/ui/minimap.lua
+++ lua/baron/plugins/ui/minimap.lua
@@ -1,5 +1,6 @@
 return {
   "wfxr/minimap.vim",
+  lazy = false,
   config = function()
     vim.g.minimap_width = 10
     vim.g.minimap_auto_start = 0
@@ -9,26 +10,24 @@ return {
 
 
     vim.cmd([[
-          autocmd BufWinEnter * highlight minimapCursor                           ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#297291
-          autocmd BufWinEnter * highlight minimapRange                            ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#547B8C
+      autocmd VimEnter * highlight minimapCursor                           ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#297291
+      autocmd VimEnter * highlight minimapRange                            ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#547B8C
 
-          autocmd BufWinEnter * highlight minimapDiffLine           cterm=italic  ctermbg=59  ctermfg=228               guifg=#9F00B8
-          autocmd BufWinEnter * highlight minimapCursorDiffLine                   ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#DD00FF
-          autocmd BufWinEnter * highlight minimapRangeDiffLine                    ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#9F00B8
+      autocmd VimEnter * highlight minimapDiffLine           cterm=italic  ctermbg=59  ctermfg=228               guifg=#9F00B8
+      autocmd VimEnter * highlight minimapCursorDiffLine                   ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#DD00FF
+      autocmd VimEnter * highlight minimapRangeDiffLine                    ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#9F00B8
 
-          autocmd BufWinEnter * highlight minimapDiffAdded          cterm=italic  ctermbg=59  ctermfg=228               guifg=#36CC00
-          autocmd BufWinEnter * highlight minimapCursorDiffAdded                  ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#44FF00
-          autocmd BufWinEnter * highlight minimapRangeDiffAdded                   ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#36CC00
+      autocmd VimEnter * highlight minimapDiffAdded          cterm=italic  ctermbg=59  ctermfg=228               guifg=#36CC00
+      autocmd VimEnter * highlight minimapCursorDiffAdded                  ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#44FF00
+      autocmd VimEnter * highlight minimapRangeDiffAdded                   ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#36CC00
 
-          autocmd BufWinEnter * highlight minimapDiffRemoved        cterm=italic  ctermbg=59  ctermfg=228               guifg=#BA0000
-          autocmd BufWinEnter * highlight minimapCursorDiffRemoved                ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#FF0000
-          autocmd BufWinEnter * highlight minimapRangeDiffRemoved                 ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#BA0000
-        ]])
+      autocmd VimEnter * highlight minimapDiffRemoved        cterm=italic  ctermbg=59  ctermfg=228               guifg=#BA0000
+      autocmd VimEnter * highlight minimapCursorDiffRemoved                ctermbg=59  ctermfg=228 guibg=#131F1B guifg=#FF0000
+      autocmd VimEnter * highlight minimapRangeDiffRemoved                 ctermbg=242 ctermfg=228 guibg=#0E1617 guifg=#BA0000
+    ]])
 
-    vim.g.minimap_diffadd_color = minimapDiffAdded
-    vim.g.minimap_diffremove_color = minimapDiffRemoved
-    vim.g.minimap_diff_color = minimapDiffLine
-
-    vim.keymap.set('n', '<leader>mt', ':MinimapToggle<CR>')
+    local keyset = require("baron.core.keymaps").set
+    local plugin = "Minimap"
+    keyset(plugin, 'n', '<leader>mt', ':MinimapToggle<CR>', { desc = "Toggle" })
   end
 }
--- lua/baron/plugins/ui/web-devicons.lua
+++ lua/baron/plugins/ui/web-devicons.lua
@@ -1,7 +1,51 @@
 return {
   "nvim-tree/nvim-web-devicons",
+  lazy = false,
   config = function()
-    require("nvim-web-devicons").set_icon({
+    local web_devicons = require("nvim-web-devicons")
+    web_devicons.setup({
+      -- your personnal icons can go here (to override)
+      -- you can specify color or cterm_color instead of specifying both of them
+      -- DevIcon will be appended to `name`
+      override = {
+        zsh = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Zsh"
+        }
+      },
+      -- globally enable different highlight colors per icon (default to true)
+      -- if set to false all icons will have the default icon's color
+      color_icons = true,
+      -- globally enable default icons (default to false)
+      -- will get overriden by `get_icons` option
+      default = true,
+      -- globally enable "strict" selection of icons - icon will be looked up in
+      -- different tables, first by filename, and if not found by extension; this
+      -- prevents cases when file doesn't have any extension but still gets some icon
+      -- because its name happened to match some extension (default to false)
+      strict = true,
+      -- same as `override` but specifically for overrides by filename
+      -- takes effect when `strict` is true
+      override_by_filename = {
+        [".gitignore"] = {
+          icon = "",
+          color = "#f1502f",
+          name = "Gitignore"
+        }
+      },
+      -- same as `override` but specifically for overrides by extension
+      -- takes effect when `strict` is true
+      override_by_extension = {
+        ["log"] = {
+          icon = "",
+          color = "#81e043",
+          name = "Log"
+        }
+      },
+    })
+    web_devicons.set_icon({
       gql = {
         icon = "",
         color = "#e535ab",
@@ -9,5 +53,10 @@ return {
         name = "GraphQL",
       },
     })
+
+    web_devicons.refresh()
+
+    -- fix icons sometimes not appearing due to lazy loading
+    vim.cmd([[autocmd VimEnter * lua require("nvim-web-devicons").refresh()]])
   end,
 }
