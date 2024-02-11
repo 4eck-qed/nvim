14c9d6c0e5ec66cd6d6bff74e22091fb1f888a3a
4eck-qed <ssindustries@web.de>
2024-02-11 02:48:22 +0100

    enable snippets

--- lua/baron/plugins/core/snippets.lua
+++ lua/baron/plugins/core/snippets.lua
@@ -12,14 +12,16 @@ return {
             local ls = require("luasnip")
             ls.filetype_extend("javascript", { "jsdoc" })
 
-            vim.keymap.set({ "i" }, "<C-e>", function() ls.expand() end, { silent = true })
-            vim.keymap.set({ "i", "s" }, "<C-[>", function() ls.jump(1) end, { silent = true })
-            vim.keymap.set({ "i", "s" }, "<C-]>", function() ls.jump(-1) end, { silent = true })
-            vim.keymap.set({ "i", "s" }, "<C-E>", function()
+            local keyset = require("baron.core.keymaps").set
+            local plugin = "LuaSnip"
+            keyset(plugin, { "i" }, "<C-e>", function() ls.expand() end, { silent = true, desc = "Expand" })
+            keyset(plugin, { "i", "s" }, "<C-[>", function() ls.jump(1) end, { silent = true, desc = "Next" })
+            keyset(plugin, { "i", "s" }, "<C-]>", function() ls.jump(-1) end, { silent = true, desc = "Prevous" })
+            keyset(plugin, { "i", "s" }, "<C-E>", function()
                 if ls.choice_active() then
                     ls.change_choice(1)
                 end
-            end, { silent = true })
+            end, { silent = true, desc = "Change choice" })
         end,
     }
 }
